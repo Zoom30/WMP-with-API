016273faf4e8f0669eff2cedc0094d5da71e13bd
Zoom30 <63873134+Zoom30@users.noreply.github.com>
2022-06-12 11:30:51 +0100

    deployment changes

--- WMP Backend/src/service.py
+++ WMP Backend/src/service.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import requests
+from src.settings import settings
 from src.data_database import DBDataAccess
 from mangum import Mangum
 from src.data_user import UserDataAccess
@@ -20,25 +22,35 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/set_initial_details")
+@app.post("/set-initial-details")
 def set_initial_details(weight: Decimal, height: Decimal):
     data = DBDataAccess()
     return data.set_initial_details(weight, height)
 
-@app.get("/initial_details")
+@app.get("/initial-details")
 def get_initial_details():
     data = DBDataAccess()
     items = data.get_initial_details().get('Items')
     return items
 
-@app.post("/sign_up")
+@app.post("/sign-up")
 def sign_up(user_name: str, password: str, email: str):
     data = UserDataAccess()
     return data.sign_up(user_name, password, email)
 
-@app.post("/sign_in")
+@app.post("/sign-in")
 def sign_in(email: str, password: str):
     data = UserDataAccess()
     return data.sign_in(email, password)
+
+@app.get("/companies-house")
+def get_info_companies_house(company_name: str):
+    COMPANIES_HOUSE_ENDPOINT = "https://api.company-information.service.gov.uk"
+    url = COMPANIES_HOUSE_ENDPOINT + "/search?q={}"
+    auth = (settings.api_key, "")
+    response = requests.get(url=url.format(company_name), auth=auth)
+    response.raise_for_status()
+    data = response.json()
+    return data
 
 handler = Mangum(app)
--- WMP Backend/src/settings.py
+++ WMP Backend/src/settings.py
@@ -1,9 +1,11 @@
+from typing import Optional
 from pydantic import BaseSettings
 
 class CommonSettings(BaseSettings):
     client_id: str
     user_pool_id: str
     client_secret: str=None
+    api_key: Optional[str] = None
 
 
 settings = CommonSettings()
